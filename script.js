e248565a68bc9a9851aeb33327ce234920ef037b
MOQRAN <mohammedmoqran18@gmail.com>
2022-05-15 21:04:33 +0200

    index

---
+++ script.js
@@ -0,0 +1,253 @@
+let langdata = {
+    "languages": {
+        "ar": {
+            "strings": {
+                "Ar": "العربية",
+                "En": "الإنجليزية",
+                "Fr": "الفرنسية",
+                "title": "مهندس إعلاميات متدرب",
+                "name": "محمد مقران",
+                "email": "البريد الإلكتروني",
+                "tel": "الهاتف",
+                "age": "السن",
+                "exp": "الخبرة العملية",
+                "exp1": "(فرنسا) FDC مهندس تطوير برمجيات",
+                "det1": "سبتمبر 2021 - أغسطس 2023",
+                "desc1": `
+                 JAVA تطوير البرمجيات 
+                ,
+                SPARX EA تطوير الوظائف الإضافية لأداة`,
+                "exp2":" تدريب محلل بيانات لنيل الإجازة المهنية المدرسة العليا للتكنولوجيا بوجدة (المغرب) ",
+                "det2":"مارس 2020 - يونيو 2020 ",
+                "desc2":`
+                دراسة مقارنة لطريقتين التصنيف
+                        تحت الإشراف: الشبكات العصبية وشجرة القرار على
+                        بيانات سريرية.
+                         RStudio تطوير لوحة العرض باستخدام .
+                        تفسير النتائج الإحصائية.
+                `,
+                "exp3":"تدريب مطور الويب، مجلس جهت الشرق (المغرب)",
+                "det3":"أبريل 2019 - يونيو 2019",
+                "desc3":`
+                .إنشاء تطبيق  لإدارة الإجازات
+                 
+                UML, Merise, PHP ,JS , HTML / CSS , Bootstrap , MySQL: التقنيات 
+                `,
+                "edu":"المسار الدراسي",
+                "edu1":"Sup Galilée مدرسة الهندسة",
+                "eduDet1":"درجة هندسة الكمبيوتر (تدريب مهني)",
+                "eduDat1":"2023 - 2020",
+                "edu2":"(المدرسة العليا للتكنولوجيا بوجدة (المغرب",
+                "eduDet2":"الإجازة المهنية في إعلاميات اتخاذ القرارات",
+                "eduDat2":"2020 - 2019",
+                "edu3":"(المدرسة العليا للتكنولوجيا بوجدة (المغرب",
+                "eduDet3":"الدبلوم التقني الجامعي في تطوير البرمجيات",
+                "eduDat3":"2019 - 2017",
+                "prj":"المشاريع",
+                "prj1":" Android تطبيق ",
+                "prjDet1":"(Android , MySQL) تطبيق للمعلومات الطبية باستخدام قاعدة بيانات ",
+               
+                "prj2":"التطبيق سطح المكتب",
+                "prjDet2":"(C# , MySQL) تطبيق سطح المكتب  لإدارة المطاعم باستخدام قاعدة بيانات",
+               
+                "prj3":"تطبيق الويب",
+                "prjDet3":"(PHP/JS ,MySQL ) تطبيق ويب  لإدارة متجر البصريات بقاعدة بيانات ",
+                
+                "comp":"المهارات",
+                "languages":"اللغات"
+
+            }
+
+        },
+        "en": {
+            "strings": {
+                "Ar": "Arab",
+                "En": "English",
+                "Fr": "French",
+                "title": "Apprentice computer engineer",
+                "name": "Mohammed MOQRAN",
+                "email": "Email",
+                "tel": "Tel",
+                "age": "Age",
+                "exp": "Professional experience",
+                "exp1": "Development engineer at FDC (France)",
+                "det1": "September 2021 - August 2023",
+                "desc1": `Software development in JAVA
+                ,
+                         Development of Add-Ins for the SPARX EA tool`,
+                "exp2":"LP Data Analyst Internship at ESTO (Morocco)",
+                "det2":"March 2020 - June 2020",
+                "desc2":`
+                        Comparative study of the two classification methods
+                        supervised: neural networks and decision tree on
+                        clinical data.
+                        Dashboard development with RStudio.
+                        Interpretation of statistical results.
+                `,
+                "exp3":" Web Developer Internship at CRO (Morocco)",
+                "det3":"April 2019 - June 2019",
+                "desc3":`
+                Creation of a web-based solution for leave management.
+                Functional and technical analysis.
+                Design with UML and Merise.
+                Technologies : PHP, JS, HTML/CSS, Bootstrap, MySQL.
+                `,
+                "edu":"Education",
+                "edu1":"Sup Galilée engineering school",
+                "eduDet1":"Computer engineering degree (apprenticeship)",
+                "eduDat1":"2020 - 2023",
+                "edu2":"Ecole Supérieure de Technologie Oujda, Maroc",
+                "eduDet2":"Professional degree in Business Intelligence",
+                "eduDat2":"2019 - 2020",
+                "edu3":"Ecole Supérieure de Technologie Oujda, Maroc",
+                "eduDet3":"DUT, Software Development",
+                "eduDat3":"2017 - 2019",
+                "prj":"Projects",
+                "prj1":"Android app",
+                "prjDet1":"Android application (JAVA) for medical information with a MySQL database",
+               
+                "prj2":"desktop app",
+                "prjDet2":"Desktop application (C#) for restaurant management with a MySQL database.",
+               
+                "prj3":"Web application",
+                "prjDet3":"Web application (PHP/JS) for the management of an optometry store with a MySQL database",
+                "comp":"Skills",
+                "languages":"Languages"
+            }
+        },
+        "fr": {
+            "strings": {
+                "Ar": "Arabe",
+                "En": "Anglais",
+                "Fr": "Français",
+                "title": "Apprentit ingénieur informatique",
+                "name": "Mohammed MOQRAN",
+                "email": "Email",
+                "tel": "Tel",
+                "age": "Age",
+                "exp": "Experience professionnelle",
+                "exp1": "Ingénieur d\'étude et de développement chez FDC (France)",
+                "det1": "Septembre 2021 - Août 2023",
+                "desc1": `Développement de logiciel en JAVA 
+                ,
+                         Développement d\'Add-Ins pour l\'outil SPARX EA`,
+                "exp2":"Stage Data Analyst de LP chez ESTO (Maroc)",
+                "det2":"Mars 2020 - Juin 2020",
+                "desc2":`
+                        Etude comparative des deux méthodes de classification
+                        supervisée : réseaux de neurones et arbre de décision sur des
+                        données cliniques.
+                        Elaboration de tableau de bord avec RStudio.
+                        Interprétation des résultats statistiques.
+                `,
+                "exp3":" Stage Developpeur Web chez CRO (Maroc)",
+                "det3":" Avril 2019 - Juin 2019",
+                "desc3":`
+                    Création d’une solution web pour la gestion des congés.
+					Analyse fonctionnelle et technique.
+					Conception avec UML et Merise.
+					Technologies : PHP, JS, HTML/CSS, Bootstrap, MySQL.
+                `,
+                "edu":"Education",
+                "edu1":"Ecole d'ingénieur Sup Galilée",
+                "eduDet1":"Diplôme d'ingénieur informatique (en apprentissage)",
+                "eduDat1":"2020 - 2023",
+                "edu2":"Ecole Supérieure de Technologie Oujda, Maroc",
+                "eduDet2":"Licence Professionnelle en Informatique Décisionnelle",
+                "eduDat2":"2019 - 2020",
+                "edu3":"Ecole Supérieure de Technologie Oujda, Maroc",
+                "eduDet3":" DUT, Développement Informatique",
+                "eduDat3":"2017 - 2019",
+                "prj":"Projets",
+                "prj1":"Application Android",
+                "prjDet1":"Application Android (JAVA) d’information médicale avec une base de données MySQL",
+               
+                "prj2":"Application desktop",
+                "prjDet2":"Application desktop (C#) pour la gestion de restaurant avec une base de données MySQL.",
+               
+                "prj3":"Application web ",
+                "prjDet3":"Application web (PHP/JS) pour la gestion d’un magasin d\’optométrie avec une base de données MySQL.",
+                
+                "comp":"Compétences",
+                "languages":"Langues"         
+                
+
+
+            }
+        }
+    }
+
+}
+function applyStrings(containers) {
+    containers.forEach(container => {
+        //find all the elements that have data-key
+        let locale = container.getAttribute('lang');
+        //console.log('looking inside of ', locale);
+        container.querySelectorAll(`[data-key]`).forEach(element => {
+            let key = element.getAttribute('data-key');
+            //console.log(element);
+            //console.log(key);
+            let lang = locale.substr(0, 2); //first 2 characters
+            if (key) {
+                element.textContent = langdata.languages[lang].strings[key];
+            }
+        });
+    })
+}
+
+function findLocaleMatch() {
+    let keys = Object.keys(langdata.languages); //from our data
+    let locales = Intl.getCanonicalLocales(keys); //from our data validated
+
+    let lang = navigator.language; //from browser 
+    let locale = Intl.getCanonicalLocales(lang); //from browser validated
+    console.log('browser language', lang);
+    console.log('locales from data file', locale);
+
+    //find the match for locale inside locales
+    let langMatch = document.documentElement.getAttribute('lang'); //default
+    locales = locales.filter(l => locale == l);
+    langMatch = (locales.length > 0) ? locales[0] : langMatch;
+    return langMatch;
+}
+
+function changeLang() {
+
+    let langValue = document.getElementById("lang").value;
+    console.log("option: ", langValue);
+    document.getElementById("cv").lang = langValue;
+    console.log("cv: ", document.getElementById("cv").lang);
+
+    const style = document.createElement('style');
+    if(langValue==="ar"){
+       
+        // style.innerHTML = `
+        //       body {
+        //         text-align: right;
+        //       }
+        //     `;
+       let html=document.getElementsByTagName("html");
+       html.dir="rtl";
+       console.log(html.dir);
+        
+    }
+    else{
+        style.innerHTML = `
+        body {
+          text-align: left;
+          margin-right: 150px;
+        }
+      `;
+
+    }
+    document.head.appendChild(style);
+
+    //skip the lang value in the HTML tag for this example
+    let zones = document.querySelectorAll('html [lang]');
+    applyStrings(zones);
+
+    let lang = findLocaleMatch();
+    let container = document.querySelector(`html [lang*=${lang}]`);
+    if (container)
+        container.className = 'lang-match';
+}
